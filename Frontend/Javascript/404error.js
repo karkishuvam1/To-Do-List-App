395af261b60670b61d478fb718513d500d17d2ef
karkishuvam1 <shuvamkarki69@gmail.com>
2026-06-18 09:58:51 +0545

    made some changes in 404 css and js file

--- Frontend/Javascript/404error.js
+++ Frontend/Javascript/404error.js
@@ -1,9 +1,4 @@
-/* ================================
-   DoIt — 404 Error Page JavaScript
-   Handles: back button
-   ================================ */
-
-/* ── Back button ── */
+/*  Back button  */
 document.getElementById('backBtn').addEventListener('click', function () {
   window.history.back();
 });
