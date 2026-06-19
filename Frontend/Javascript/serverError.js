8bb435d564eef9809ac07af3962cc20701691f26
karkishuvam1 <shuvamkarki69@gmail.com>
2026-06-19 09:10:18 +0545

    made server error page and session expired page

---
+++ Frontend/Javascript/serverError.js
@@ -0,0 +1,4 @@
+/*Back button*/
+document.getElementById('backBtn').addEventListener('click', function () {
+  window.history.back();
+});
---
+++ Frontend/Javascript/sessionExpired.js
@@ -0,0 +1,4 @@
+/*Back button — redirects to login*/
+document.getElementById('backBtn').addEventListener('click', function () {
+  window.location.href = 'login.html';
+});
